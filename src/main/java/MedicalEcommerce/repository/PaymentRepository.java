e01aff67c6668ac7d2b37901be404dcf2d0dc5f3
D1DEBASIS <debasisdash2000@gmail.com>
2026-03-28 16:46:59 +0530

    Delete src directory

--- src/main/java/MedicalEcommerce/repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package MedicalEcommerce.repository;
-
-import MedicalEcommerce.model.PaymentDtls;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<PaymentDtls, String> {
-
-}
